adb8e576cdfd187c81f6a08f44ee5045ffad4341
DeepSandhra <109024247+DeepSandhra@users.noreply.github.com>
2022-08-29 16:40:58 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,9 +1,9 @@
 # Bot information
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
+API_ID = 17976549
+API_HASH = 'fd317a84774beefb478feb6a37e442f7'
+BOT_TOKEN = '5584674817:AAEE4IFnUDoZps_TqgCWijohlGbWn4doCjk'
 USERBOT_STRING_SESSION = ''
 
 # Bot settings
@@ -11,14 +11,14 @@ CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
+ADMINS = [1658586400, 'admin123', 1658586400]
+CHANNELS = [-1001721860031, 'allMoviesavailable_2022']
 AUTH_USERS = []
-AUTH_CHANNEL = None
+AUTH_CHANNEL = -1001765154455
 
 # MongoDB information
-DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://Botsfortelegram1:Botsfortelegram1@cluster0.xvqpe.mongodb.net/?retryWrites=true&w=majority"
 DATABASE_NAME = 'Telegram'
-COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
+COLLECTION_NAME = 'Cynite_files'  # If you are using the same database, then use different collection name for each bot
 
 
